b294ee70155cb4f1d8dfea15c94d8ab90d98a2aa
Felipe Soczek <felipecebulskisoczek@gmail.com>
2021-10-25 15:10:52 -0300

    settings page finished

---
+++ src/pages/PageHelp.vue
@@ -0,0 +1,21 @@
+<template>
+  <q-page padding>
+    <q-btn
+      to="/settings"
+      color="primary"
+      icon="chevron_left"
+      label="Back"
+      flat
+    >
+    </q-btn>
+
+    <h5>How to use Awesome Todo</h5>
+    <p>Lorem ipsum dolor sit amet, consectetur</p>
+  </q-page>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
--- src/pages/PageSettings.vue
+++ src/pages/PageSettings.vue
@@ -1,6 +1,6 @@
 <template>
   <q-page padding>
-    <q-list bordered padding>
+    <q-list class="q-mb-md" bordered padding>
       <q-item-label header>Settings</q-item-label>
 
       <q-item tag="label" v-ripple>
@@ -21,11 +21,55 @@
         </q-item-section>
       </q-item>
     </q-list>
+
+    <q-list bordered padding>
+      <q-item-label header>More</q-item-label>
+
+      <q-item 
+        to="/settings/help"
+        tag="label" 
+        v-ripple
+      >
+        <q-item-section>
+          <q-item-label>Help</q-item-label>
+        </q-item-section>
+        <q-item-section side>
+          <q-icon name="chevron_right" ></q-icon>
+        </q-item-section>
+      </q-item>
+
+      <q-item 
+        @click="visitOurWebsite"
+        tag="label" 
+        v-ripple
+      >
+        <q-item-section>
+          <q-item-label>Visit our website</q-item-label>
+        </q-item-section>
+        <q-item-section side>
+          <q-icon name="chevron_right" ></q-icon>
+        </q-item-section>
+      </q-item>
+
+      <q-item 
+        @click="emailUs"
+        tag="label" 
+        v-ripple
+      >
+        <q-item-section>
+          <q-item-label>Email Us</q-item-label>
+        </q-item-section>
+        <q-item-section side>
+          <q-icon name="chevron_right" ></q-icon>
+        </q-item-section>
+      </q-item>
+    </q-list>
   </q-page>
 </template>
 
 <script>
 import { mapGetters, mapActions } from 'vuex'
+import { openURL } from 'quasar'
 
 export default {
   computed: {
@@ -51,7 +95,13 @@ export default {
     ...mapActions('settings', [
       'setShow12HourTimeFormat',
       'setShowTasksInOneList'
-    ])
+    ]),
+    visitOurWebsite() {
+      openURL('http://www.google.com')
+    },
+    emailUs() {
+      window.location.href = 'mailto:hello@awesometodo.com?subject=Awesome Todo Feedback'
+    }
   }
 }
 </script>
